18613b0480b959beb0f589ea8851158c577a5283
WitnessOfThe <puzurdan@gmail.com>
2023-02-22 14:19:42 +0000

    Mil_3_cleaning_data_resize_im and readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Facebook Marketplace Recommendation Ranking System
+#1 data preparation. 
+
+We have two ".csv" tables and one archive with the graphical data. The text data "Products.csv" contains information about listings on the market grouped by the listing id ("product_id") with their categorisation and description, while the "Images.csv" maps the listing id with image_id stored separately in the archive as ".jpeg".
+
+The cleaning of the ".csv" data starts with the conversion of the "price" column into a proper "float" and removing all raws consisting of missing or NaN data from the dataset "Products.csv". 
+The field "category" contains the hierarchical structure separated by " / ", we parse it to extract the root category. 
+The root category has to be transformed into the number, so dictionaries "encoder" and "decoder" is implemented.
+Finally, the categories is mapped to the image id from "Images.csv", through the "product_id" forming dataset "training_data.csv".
+
+The graphical data has to be homegeneus to be processed by ML algorithm, therefore initial images has to be checked to be the same "RGB" type and the same size. As we realise that all out figures has different resolution and aspect ratio, we need to fix it by resizing each image to the simular size. The class ImagesHandle is responsible for performing this task.
--- clean_images_data.py
+++ clean_images_data.py
@@ -1,26 +1,76 @@
-import chardet
-import csv
+# %%
 import pandas as pd
-import cchardet as chardet
+import numpy as np
+import cv2
+import os
+from PIL import Image
+import os
 
-class DataImages:
+class ImagesHandle:
 
-    def __init__(self,name) -> None:
-        self.name = name
-        self.df   = self.load_images_data(name)
+    def __init__(self,path_dirty,path_clean,size=512) -> None:
+        self.size       = size
+        self.path_dirty = path_dirty
+        self.path_clean = path_clean
+        self.names      = self.get_list_of_images_names(path_dirty)
+        self.start_resize()
+        pass
 
-    def load_images_data(self,name):
-        #'detect incoding'
-        with open(name, 'rb') as f:
-            result = chardet.detect(f.read())
-        encoding = result['encoding']
-        print(encoding)        
-        #'Sniff cvs and convert to pandas'       
-        with open(name, 'r',encoding=encoding,newline='') as csvfile:
-            reader   = csv.reader(  csvfile, delimiter=',',quotechar='"')# delimiter=',',quotechar='"',lineterminator='\n')#,
-            data = list(reader)
-            return pd.DataFrame(data[1:], columns=data[0])
+    def start_resize(self):
+        i = 0
+        for _ in self.names:
+            print(i)
+            i += 1
+            new_im = self.resize_image(self.size, Image.open(self.path_dirty+_))
+            new_im.save(self.path_clean+_)
+
+    def resize_image(self,final_size, im):
+        size = im.size
+        ratio = float(final_size) / max(size)
+        new_image_size = tuple([int(x*ratio) for x in size])
+        im = im.resize(new_image_size, Image.ANTIALIAS)
+        new_im = Image.new("RGB", (final_size, final_size))
+        new_im.paste(im, ((final_size-new_image_size[0])//2, (final_size-new_image_size[1])//2))
+        return new_im
+
+
+    def get_list_of_images_names(self,path):
+        self.names = os.listdir(path)
+#        print(self.names)
+        for _ in self.names:
+            if not('.jpg' in _):
+                self.names.remove(_)
+        return self.names
+
+    def get_list_of_images_size(self,path):
+        width   = []
+        height  = []
+        channel = []
+        i = 0
+        for _ in self.names:
+            print(i)
+            i += 1
+            raw = cv2.imread(path+_).shape
+            width.  append(raw[0])
+            height. append(raw[1])
+            channel.append(raw[2])
+        frame_size = pd.DataFrame({'width':width,'height':height,'channel':channel,})
+        return frame_size
+
 if __name__ == '__main__':
-    print(DataImages('Images.csv').df.head())
-    print(DataImages('Images.csv').df.info())
+    path_dirty = 'C:/Users/dp710/Documents/GitHub/facebook-marketplaces-recommendation-ranking-system/images_fb/images/'
+    path_clean = 'C:/Users/dp710/Documents/GitHub/facebook-marketplaces-recommendation-ranking-system/images_fb/clean_images/'
+    ih = ImagesHandle(path_dirty,path_clean)
+#    ih.get_list_of_images_names(path_dirty)
+ #   df_size = ih.get_list_of_images_size(path_dirty)
     pass
+#'''    
+    
+  #  dirs = os.listdir(path)
+ #   final_size = 512
+   # for n, item in enumerate(dirs[:5], 1):
+    #    im = Image.open('images/' + item)
+     #   new_im = resize_image(final_size, im)
+      #  new_im.save(f'{n}_resized.jpg')
+
+# %%
