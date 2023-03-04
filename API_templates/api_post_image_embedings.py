107a8ce0b18dca1f62df6a01c70a5495ce5d0294
WitnessOfThe <33790455+WitnessOfThe@users.noreply.github.com>
2023-03-04 01:57:41 +0000

    API calls

--- API_templates/api_post_image_embedings.py
+++ API_templates/api_post_image_embedings.py
@@ -2,13 +2,9 @@
 import os
 import requests
 
-if 'requests_templates' in os.getcwd():
-    os.chdir("..")
-    root_path = os.getcwd()
-
 host = 'http://127.0.0.1:8000'   # local instance adress 
 
-file_path = root_path+'\\images_fb\\images\\'      # the image from test dataset
+file_path = 'images_fb\\images\\'                # the image from test dataset
 file_name = 'ebdb09e9-de15-4b63-aff0-bae01c9cd068' # file example 
 
 url = host+'/predict/feature_embedding'
